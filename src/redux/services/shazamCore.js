7111d9d3ea3fa66a1bc2ef58d8256c62a311a1ef
Maxmilly4050 <maxsteel4050@gmail.com>
2025-06-16 14:14:45 +0300

    addon

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -4,28 +4,26 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
   export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v0',
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
         prepareHeaders: (headers) => {
-            headers.set('x-rapidapi-key', '3df89369a1msh896ceb0bc9301f4p1c673ajsn0c8416e8657d');
+            headers.set('x-rapidapi-key', '​4df89369a1msh896ceb0bc9301f4p1c673ajsn0c8416e8657d'); 
             headers.set('x-rapidapi-host', 'shazam-core.p.rapidapi.com');
             return headers;
         },
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({ query: () => '/charts/world' }),
-        getSongDetails: builder.query({ query: (songId) => `/tracks/details?track_id=${songId}` }),
-        getSongRelated: builder.query({ query: (songId) => `/tracks/related?track_id=${songId}` }),
-        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
-        getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+        // getSongDetails: builder.query({ query: (songId) => `/tracks/details?track_id=${songId}` }),
+        // getSongRelated: builder.query({ query: (songId) => `/tracks/related?track_id=${songId}` }),
+        // getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+        // getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
     })
 });
 // Export hooks for the defined endpoints
 export const {
     useGetTopChartsQuery,
-    useGetSongDetailsQuery,
-    useGetSongRelatedQuery,
-    useGetArtistDetailsQuery,
-    useGetSongsByGenreQuery
+    // useGetSongDetailsQuery,
+    // useGetSongRelatedQuery,
+    // useGetArtistDetailsQuery,
+    // useGetSongsByGenreQuery
 } = shazamCoreApi;
-// Export the API service to be used in the Redux store
-export { shazamCoreApi };
