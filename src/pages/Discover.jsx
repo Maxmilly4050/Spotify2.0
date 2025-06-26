7c23a41ec96ff405d99cff2a6eae5b7bc6821dfc
Maxmilly4050 <maxsteel4050@gmail.com>
2025-06-26 11:42:43 +0300

    loader

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,6 +6,11 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 const Discover = () => {
     const { data, isFetching, error } = useGetTopChartsQuery();
     const genreTitle = 'Pop';
+    if (isFetching) return <Loader title="Loading songs..." />;
+    if (error) return <Error />;
+
+    console.log(data, isFetching, error);
+
     return (
         <div className="flex flex-col ">
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -24,6 +29,13 @@ const Discover = () => {
             </div>
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
                 {/* Songs will be mapped here */}
+                {data?.map((song, i) => (
+                    <SongCard
+                        key={song.key}
+                        song={song}
+                        i={i}
+                    />
+                ))}
             </div>
         </div>
     );
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -4,15 +4,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
   export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        baseUrl: 'https://shazam-core.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-            headers.set('x-rapidapi-key', '​4df89369a1msh896ceb0bc9301f4p1c673ajsn0c8416e8657d'); 
+            headers.set('x-rapidapi-key','4df89369a1msh896ceb0bc9301f4p1c673ajsn0c8416e8657d'); 
             headers.set('x-rapidapi-host', 'shazam-core.p.rapidapi.com');
             return headers;
         },
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => '/charts/world' }),
+        getTopCharts: builder.query({ query: () => 'v1/charts/genre-world?genre_code=POP&country_code=DZ' }),
         // getSongDetails: builder.query({ query: (songId) => `/tracks/details?track_id=${songId}` }),
         // getSongRelated: builder.query({ query: (songId) => `/tracks/related?track_id=${songId}` }),
         // getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
